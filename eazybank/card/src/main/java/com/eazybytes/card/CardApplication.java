1be839be96d9a2585b9e6772b5da80943314944d
NileshNSwami7 <nilnswami110@gmail.com>
2026-04-10 00:22:52 +0530

    Handle CardAlreadyExistException,ResourceNotFOundException and GlobalException.

--- eazybank/card/src/main/java/com/eazybytes/card/CardApplication.java
+++ eazybank/card/src/main/java/com/eazybytes/card/CardApplication.java
@@ -1,8 +1,11 @@
 package com.eazybytes.card;
 
+import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @SpringBootApplication
 public class CardApplication {
 
