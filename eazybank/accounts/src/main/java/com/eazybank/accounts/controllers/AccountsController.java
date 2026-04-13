386862739b9b52168043c24b5dc2ab91b9b60657
NileshNSwami7 <nilnswami110@gmail.com>
2026-04-13 22:00:56 +0530

    add Environment interface and create REST API to check versions.

--- eazybank/accounts/src/main/java/com/eazybank/accounts/controllers/AccountsController.java
+++ eazybank/accounts/src/main/java/com/eazybank/accounts/controllers/AccountsController.java
@@ -7,7 +7,9 @@ import com.eazybank.accounts.services.IAccountsService;
 import io.swagger.v3.oas.annotations.Operation;import io.swagger.v3.oas.annotations.media.Content;import io.swagger.v3.oas.annotations.media.Schema;import io.swagger.v3.oas.annotations.responses.ApiResponse;import io.swagger.v3.oas.annotations.responses.ApiResponses;import io.swagger.v3.oas.annotations.tags.Tag;import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Sort;import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +33,9 @@ public class AccountsController {
     }
     @Value("${build.version}")
     private String buildVersion;
+
+    @Autowired
+    private Environment environment;
 
 
     @Operation(
@@ -153,8 +158,8 @@ public class AccountsController {
     }
 
     @Operation(
-            summary = "Fetch build info REST API",
-            description = "REST API to FETCH the build version info"
+            summary = "Fetch java version info REST API",
+            description = "REST API to FETCH the java version info"
     )
     @ApiResponses({
             @ApiResponse(
@@ -173,5 +178,28 @@ public class AccountsController {
     public ResponseEntity<String>getBuildInfo(){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(buildVersion);
+    }
+
+    @Operation(
+            summary = "Fetch build info REST API",
+            description = "REST API to FETCH the build version info"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
+    @GetMapping("/java-version")
+    public ResponseEntity<String>getJavaversion(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(environment.getProperty("HOME_JAVA"));
     }
 }
