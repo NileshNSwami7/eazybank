0ed614fcb8ebd6d168d233dc2e5c6e8766c96d66
NileshNSwami7 <nilnswami110@gmail.com>
2026-05-02 22:52:23 +0530

    Create security filterchain in gatewayserver.

---
+++ eazybank/gatewayserver/src/main/java/com/eazybank/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.eazybank.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springFilterchain(ServerHttpSecurity  serverHttpSecurity){
+        serverHttpSecurity.authorizeExchange(exchange->exchange.pathMatchers(HttpMethod.GET).permitAll()
+                .pathMatchers("eazybank/accounts/**").authenticated()
+                .pathMatchers("eazbank/loans/**").authenticated()
+                .pathMatchers("eazbank/cards/**").authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                        .jwt(Customizer.withDefaults()));
+        serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
+        return serverHttpSecurity.build();
+    }
+}
